a77fd2f48ed6ea765cf9679055706d35bb7df443
LZhang <edzhang2004@gmail.com>
2018-12-28 13:05:06 -0800

    version 2.0

--- functions.py
+++ functions.py
@@ -1,2 +1,11 @@
 def fuction1():
-    pass
+    print ("my python function")
+    return
+
+def printname(s):
+    print("the name is %s" %(s))
+name="edward zhang"
+printname(name)
+
+def printname( score, s = 'no name'):
+    print("the is %s and the score is %d" %(name, score))
